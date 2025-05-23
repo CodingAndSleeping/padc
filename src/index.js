e463ac84ddea98adc9124a52e876bcaca377b0cc
lzt <1162300237@qq.com>
2025-05-23 20:36:52 +0800

    feat: add options

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ import minimist from 'minimist';
 import YAML from 'yaml';
 import npa from 'npm-package-arg';
 import ora from 'ora';
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+
 import log from './log.js';
 
 const args = process.argv.slice(2);
@@ -15,18 +18,10 @@ const args = process.argv.slice(2);
 // 格式化参数
 const foramtArgs = minimist(args);
 
-// 分类名
-const catalogName = foramtArgs.c;
-// 要安装的模块
-const packages = foramtArgs._;
-
-// 要透传给 pnpm add 的参数，过滤掉 -c 和 catalogName
-const passThroughArgs = args.filter(
-  (arg) => arg !== '-c' && arg !== catalogName,
-);
-
 // 是否是开发依赖
 const isDev = foramtArgs.D || foramtArgs['save-dev'];
+
+const packages = foramtArgs._;
 
 const pkgJsonPath = join(process.cwd(), 'package.json');
 const pnpmWorkspaceYamlPath = join(process.cwd(), 'pnpm-workspace.yaml');
@@ -54,86 +49,154 @@ const pnpmWorkspaceYaml =
 // 读取 package.json 文件
 const pkgJson = JSON.parse(readFileSync(pkgJsonPath, 'utf-8'));
 
+const pkgDependencies = isDev
+  ? pkgJson.devDependencies || (pkgJson.devDependencies = {})
+  : pkgJson.dependencies || (pkgJson.dependencies = {});
+
+const packagesInfo = [];
+
 try {
-  const pkgDependencies = isDev
-    ? pkgJson.devDependencies || (pkgJson.devDependencies = {})
-    : pkgJson.dependencies || (pkgJson.dependencies = {});
-
   for (let i = 0; i < packages.length; i++) {
-    const { name: packageName, raw } = npa(packages[i]);
+    const raw = packages[i];
+    const { name: packageName } = npa(raw);
 
-    const packageVersion = raw.replace(packageName, '').split('@')[1];
+    const catalogName =
+      foramtArgs.c || (await selectCatalog(pnpmWorkspaceYaml, packageName));
+
+    const version = getVersion(
+      pnpmWorkspaceYaml,
+      raw,
+      catalogName,
+      packageName,
+    );
 
     // 更新 pnpmWorkspaceYaml
-    if (catalogName) {
+    if (catalogName !== 'default') {
       pnpmWorkspaceYaml.catalogs = pnpmWorkspaceYaml.catalogs || {};
       pnpmWorkspaceYaml.catalogs[catalogName] =
         pnpmWorkspaceYaml.catalogs[catalogName] || {};
-
-      if (packageVersion) {
-        pnpmWorkspaceYaml.catalogs[catalogName][packageName] = packageVersion;
-      } else {
-        if (!pnpmWorkspaceYaml.catalogs[catalogName][packageName]) {
-          const latestVersion = getLatestVersion(packageName);
-          pnpmWorkspaceYaml.catalogs[catalogName][packageName] = latestVersion
-            ? '^' + latestVersion
-            : '*';
-        }
-      }
-      // 更新 dependencies
+      pnpmWorkspaceYaml.catalogs[catalogName][packageName] = version;
       pkgDependencies[packageName] = `catalog:${catalogName}`;
     } else {
       pnpmWorkspaceYaml.catalog = pnpmWorkspaceYaml.catalog || {};
-
-      if (packageVersion) {
-        pnpmWorkspaceYaml.catalog[packageName] = packageVersion;
-      } else {
-        if (!pnpmWorkspaceYaml.catalog[packageName]) {
-          const latestVersion = getLatestVersion(packageName);
-          pnpmWorkspaceYaml.catalog[packageName] = latestVersion
-            ? '^' + latestVersion
-            : '*';
-        }
-      }
-      // 更新 dependencies
+      pnpmWorkspaceYaml.catalog[packageName] = version;
       pkgDependencies[packageName] = 'catalog:';
     }
+
+    packagesInfo.push({
+      packageName,
+      catalogName,
+      version,
+    });
   }
 } catch (err) {
-  log.error('Failed to get package version with pnpm.');
+  log.error('Failed to update version.', err);
   process.exit(1);
 }
-// 写入 pnpm-workspace.yaml 文件
-writeFileSync(pnpmWorkspaceYamlPath, YAML.stringify(pnpmWorkspaceYaml));
-
-// 更新 package.json
-writeFileSync(pkgJsonPath, JSON.stringify(pkgJson, null, 2));
 
 try {
+  // 写入 pnpm-workspace.yaml 文件
+  writeFileSync(pnpmWorkspaceYamlPath, YAML.stringify(pnpmWorkspaceYaml));
+
+  // 更新 package.json
+  writeFileSync(pkgJsonPath, JSON.stringify(pkgJson, null, 2));
+
+  // 要透传给 pnpm add 的参数，过滤掉 -c 和 catalogName
+  const passThroughArgs = args.filter(
+    (arg) => arg !== '-c' && arg !== foramtArgs.c,
+  );
   // 执行安装命令
   execSync(`pnpm add ${passThroughArgs.join(' ')}`, {
     stdio: 'inherit',
   });
 
-  log.success(
-    `Installed ${packages.join(', ')} ${
-      catalogName ? `into catalog: ${catalogName}` : 'into catalog: default'
-    }`,
-  );
+  packagesInfo.forEach(({ packageName, catalogName, version }) => {
+    log.success(
+      `Installed ${chalk.blue(packageName + '@' + version)} ${
+        catalogName
+          ? `into catalog: ${chalk.yellow(catalogName)}`
+          : `into catalog: ${chalk.yellow('default')}`
+      }`,
+    );
+  });
 } catch (err) {
   log.error('Failed to install packages with pnpm.');
   process.exit(1);
 }
 
-function getLatestVersion(packageName) {
-  const spinner = ora(`Fetching latest version of ${packageName}...`).start();
+function getVersion(pnpmWorkspaceYaml, raw, catalogName, packageName) {
+  const spinner = ora(
+    `get the version of ${chalk.blue(packageName)}...`,
+  ).start();
+
   try {
-    const latestVersion = execSync(`npm view ${packageName} version`, {
-      encoding: 'utf-8',
-    }).trim();
-    spinner.succeed(`Got ${packageName}@${latestVersion}`);
-    return latestVersion;
+    const packageVersion = raw.replace(packageName, '').split('@')[1]; // 用户指定的版本号
+
+    const pnpmWorkspaceYamlVersion = getPnpmWorkspaceYamlVersion(
+      pnpmWorkspaceYaml,
+      catalogName,
+      packageName,
+    ); // pnpm-workspace.yaml 中对应 catalog 的版本号
+
+    const version =
+      packageVersion ||
+      pnpmWorkspaceYamlVersion ||
+      '^' + getLatestVersion(packageName); // 最新版本号
+
+    spinner.succeed(`Got ${packageName}@${version}`);
+
+    return version;
   } catch (err) {
-    spinner.fail(`Failed to get ${packageName} latest version`);
+    spinner.fail(`Failed to get ${packageName} version, use * instead.`);
+    return '*';
   }
+}
+
+function getPnpmWorkspaceYamlVersion(pnpmWorkspaceYaml, catalogName, name) {
+  if (catalogName === 'default') {
+    return pnpmWorkspaceYaml.catalog?.[name];
+  }
+
+  return pnpmWorkspaceYaml.catalogs?.[catalogName]?.[name];
+}
+
+function getLatestVersion(packageName) {
+  const latestVersion = execSync(`npm view ${packageName} version`, {
+    encoding: 'utf-8',
+  }).trim();
+
+  return latestVersion;
+}
+
+async function selectCatalog(pnpmWorkspaceYaml, packageName) {
+  const existingCatalogs = Object.keys(pnpmWorkspaceYaml.catalogs || {});
+
+  const catalogChoices = [
+    { name: 'default', value: 'default' },
+    ...existingCatalogs.map((name) => ({ name: `${name}`, value: name })),
+    { name: '<new catalog>', value: '__create__' },
+  ];
+
+  const { catalogChoice } = await inquirer.prompt([
+    {
+      name: 'catalogChoice',
+      type: 'list',
+      message: `select catalog for ${chalk.blue(packageName)}`,
+      choices: catalogChoices,
+    },
+  ]);
+
+  if (catalogChoice === '__create__') {
+    const { newCatalog } = await inquirer.prompt([
+      {
+        name: 'newCatalog',
+        type: 'input',
+        message: `enter new catalog name for ${chalk.blue(packageName)}：`,
+        validate: (input) => (input ? true : 'catalog name is required'),
+      },
+    ]);
+    return newCatalog;
+  }
+
+  return catalogChoice;
 }
